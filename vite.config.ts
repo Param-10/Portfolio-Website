466eb2e7edbe9ccdc4320abe09b75eeb63d63f23
Param-10 <bheleparamveer@gmail.com>
2025-06-07 21:07:22 -0400

    Update Vite configuration and HTML references for improved asset management. Add path alias for src directory and adjust base URL for production. Remove outdated JavaScript file to streamline the build process.

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/Portfolio-Website/' : '/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
